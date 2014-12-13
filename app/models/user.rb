dc9245e4a8c561ff77ce393470004299cf526dd4
reizist <reizist@gmail.com>
2014-12-13 15:34:01 +0900

    Override should_generate_new_friendly_id? method to regenerate friendly_id when update records

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   include FriendlyId
-  friendly_id :name
+  friendly_id :name, use: :slugged
+
+  private
+
+  def should_generate_new_friendly_id?
+    name_changed? || super
+  end
 end
